296321f2638a9f72e3852174566bf471d22b5dc7
pagcoinbtc <167659900+pagcoinbtc@users.noreply.github.com>
2024-08-07 06:03:44 -0300

    Update tor1.sh

--- tor1.sh
+++ tor1.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Atualiza a lista de pacotes e realiza a atualização completa do sistema
+# Atualizar a lista de pacotes e fazer um upgrade completo
 sudo apt update && sudo apt full-upgrade -y
 
-# Instala o apt-transport-https
+# Instalar apt-transport-https
 sudo apt install apt-transport-https -y
 
-# Cria o arquivo tor.list e adiciona o conteúdo necessário
-TOR_LIST_FILE="/etc/apt/sources.list.d/tor.list"
-echo "deb     [arch=amd64 signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org jammy main" | sudo tee $TOR_LIST_FILE
-echo "deb-src [arch=amd64 signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org jammy main" | sudo tee -a $TOR_LIST_FILE
+# Criar o arquivo de repositório do Tor e adicionar o conteúdo
+sudo bash -c 'cat <<EOF > /etc/apt/sources.list.d/tor.list
+deb     [arch=amd64 signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org jammy main
+deb-src [arch=amd64 signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org jammy main
+EOF'
 
-# Confirmação de sucesso
-echo "Script executado com sucesso!"
+# Instruções finais
+echo "realize: "sudo su" e cole este comando e de exit"
+echo "wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | tee /usr/share/keyrings/tor-archive-keyring.gpg >/dev/null"
